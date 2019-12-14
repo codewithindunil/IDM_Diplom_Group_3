3972ef494efa5a4aeb8061f0a198e8497f37ec25
codewithindunil <codewithindunil@gmail.com>
2019-12-14 15:55:09 +0530

    python 1st day

--- Python/1.1 UserInputs.py
+++ Python/1.1 UserInputs.py
@@ -6,5 +6,21 @@ subThree=eval(input('Enter Your Sub Three Marks : '))
 subFour=eval(input('Enter Your Sub Four Marks : '))
 subFive=eval(input('Enter Your Sub Five Marks : '))
 
+#GETTING TOTAL OF MARKS
 total=subOne+subTwo+subThree+subFour+subFive
 print('Hello ,',name,' Your Total Is : ',total)
+
+#GETTING AVERAGE
+average=total/5
+print('Your Average Is : ',average)
+
+
+
+if average>75:
+    print('You Have A Pass')
+elif average>65:
+    print('You Have B Pass')
+elif average>55:
+    print('You Have C Pass')
+else:
+    print('You Are Fail')
